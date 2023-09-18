f5c087407ab8b8a3070f9ab73657e9820738aa3c
Chukwuemeka Eze <c.eze@outlook.com>
2023-09-18 14:57:57 +0100

    sorting issues about wrong school for push to Chuka 1.19

--- app.js
+++ app.js
@@ -510,8 +510,8 @@ async function matchUTMECandidateHashSaved(type,tableName, toSendSample, phone) 
         //           type2 = toSend[0]['phone'] ? toSend[0]['phone'] : 0
         // console.log('r1::', r1)
         // console.log('r1[0]::', r1[0])
-        console.log('this is phone::', (phone))
-        console.log('this is phone function::', isNullOrUndefined(phone))
+        // console.log('this is phone::', (phone))
+        // console.log('this is phone function::', isNullOrUndefined(phone))
 
 
         if (r1.length > 0) {
@@ -556,8 +556,8 @@ async function matchUTMECandidateHashSaved(type,tableName, toSendSample, phone) 
             const h2 = crypto.createHash('sha1').update(`${JSON.stringify(r1[0])}`).digest('hex')
 
 
-            console.log("r1 from SavedTable::",r1[0])
-            console.log("newJSON from to save",newJSON)
+            // console.log("r1 from SavedTable::",r1[0])
+            // console.log("newJSON from to save",newJSON)
             // console.log('this is phone::', (phone))
             // console.log('this is phone to string::', (phone).toString())
 
@@ -2268,8 +2268,8 @@ async function onStudentsRecordSendSave(req, res) {
                 projectManagers = []
                 itemNo = 0
                 // await waitForServerProcess(delays)
-                const answerToken = {}
-                // const answerToken = await postChukaBatch(copyprojectManagers, issuesBatches)
+                // const answerToken = {}
+                const answerToken = await postChukaBatch(copyprojectManagers, issuesBatches)
                 // if successful
                 if (answerToken.status) 
                 {
@@ -2300,8 +2300,8 @@ async function onStudentsRecordSendSave(req, res) {
                 batchCondition[0] = currentBatch
                 // await waitForServerProcess(delays)
                 const copyprojectManagers = projectManagers
-                const answerToken = {}
-                // const answerToken = await postChukaBatch(copyprojectManagers, issuesBatches)// const waitanswer = await waitForServerProcess(delays)
+                // const answerToken = {}
+                const answerToken = await postChukaBatch(copyprojectManagers, issuesBatches)// const waitanswer = await waitForServerProcess(delays)
                 console.log('total number sent in this batch::', copyprojectManagers.length)
                 if (answerToken.status) 
                 {
